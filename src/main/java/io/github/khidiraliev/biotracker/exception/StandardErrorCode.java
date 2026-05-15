951c58805e8e266878ebcdf523dafed6a743e861
Alexander Kh <khidiralievalexander@gmail.com>
2026-05-15 11:01:47 +0300

    Moved VALIDATION_ERROR to new line.

--- src/main/java/io/github/khidiraliev/biotracker/exception/StandardErrorCode.java
+++ src/main/java/io/github/khidiraliev/biotracker/exception/StandardErrorCode.java
@@ -2,7 +2,8 @@ package io.github.khidiraliev.biotracker.exception;
 
 public enum StandardErrorCode implements ErrorCode {
     INTERNAL_ERROR,
-    RESOURCE_NOT_FOUND, VALIDATION_ERROR;
+    RESOURCE_NOT_FOUND,
+    VALIDATION_ERROR;
 
     @Override
     public String getCode() {
